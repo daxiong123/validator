f14ff2bdade416c05884af2747869667db38b96e
我叫张大熊 <phperweb@gmail.com>
2016-09-18 10:55:17 +0800

    To complete the development.

---
+++ src/Validator.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Aichong;
+
+/**
+ * Class Validator
+ *
+ * @package Aichong
+ */
+class Validator
+{
+
+    /**
+     * 错误消息
+     *
+     * @var string
+     */
+    private $message = '';
+
+    /**
+     * 数据验证
+     *
+     * @param array $data
+     * @param array $rule
+     * @param array $message
+     * @return bool
+     */
+    public function validate(array $data, array $rule, array $message)
+    {
+        foreach ($rule as $key => $val) {
+
+            $temp = explode('|', $val);
+            $params = $temp;
+
+            foreach ($params as $value) {
+
+                if (!array_key_exists($key, $rule)) {
+
+                    continue;
+                }
+
+                $args = [$data[$key]];
+
+                if (strpos($value, ':') !== false) {
+
+                    $tmp = explode(':', $value);
+
+                    $func = '_' . $tmp[0];
+                    array_push($args, $tmp[1]);
+
+                    unset($tmp);
+                } else {
+
+                    $func = '_' . $value;
+                }
+
+                $ret = call_user_func_array([$this, $func], $args);
+
+                if ($ret === false) {
+
+                    $this->message = vsprintf($message[$key], $args);
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 返回错误信息
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->message;
+    }
+
+    /**
+     * 验证必填
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _required($value)
+    {
+        return !empty($value) ? true : false;
+    }
+
+    /**
+     * 等于
+     *
+     * @param string $value
+     * @param string $value1
+     * @return bool
+     */
+    private function _equals($value, $value1)
+    {
+        return $value == $value1 ? true : false;
+    }
+
+    /**
+     * 长度
+     *
+     * @param mixed $value
+     * @param int   $length
+     * @return bool
+     */
+    private function _length($value, $length)
+    {
+        return strlen($value) == (int)$length ? true : false;
+    }
+
+    /**
+     * 验证URL
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _url($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_URL) !== false ? true : false;
+    }
+
+    /**
+     * 验证最小值
+     *
+     * @param mixed $value
+     * @param int   $length
+     * @return bool
+     */
+    private function _min($value, $length)
+    {
+        if (is_int($value)) {
+
+            return $value >= (int)$length ? true : false;
+        } else {
+
+            return strlen($value) >= (int)$length ? true : false;
+        }
+    }
+
+    /**
+     * 验证最大值
+     *
+     * @param mixed $value
+     * @param int   $length
+     * @return bool
+     */
+    private function _max($value, $length)
+    {
+        if (is_int($value)) {
+
+            return $value <= (int)$length ? true : false;
+        } else {
+
+            return strlen($value) <= (int)$length ? true : false;
+        }
+    }
+
+    /**
+     * 验证IPv4地址
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _ip($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false ? true : false;
+    }
+
+    /**
+     * 验证邮箱
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _email($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false ? true : false;
+    }
+
+    /**
+     * 验证手机号码
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _phone($value)
+    {
+        return preg_match('#^(13|14|15|18|17)[0-9]{9}$#', $value) ? true : false;
+    }
+
+    /**
+     * 验证身份证号码
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function _id_card($value)
+    {
+        // 只能是18位
+        if (strlen($value) != 18) {
+
+            return false;
+        }
+
+        // 取出本体码
+        $idcard_base = substr($value, 0, 17);
+
+        // 取出校验码
+        $verify_code = substr($value, 17, 1);
+
+        // 加权因子
+        $factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+
+        // 校验码对应值
+        $verify_code_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+
+        // 根据前17位计算校验码
+        $total = 0;
+        for ($i = 0; $i < 17; $i++) {
+
+            $total += substr($idcard_base, $i, 1) * $factor[$i];
+        }
+
+        // 取模
+        $mod = $total % 11;
+
+        // 比较校验码
+        return $verify_code == $verify_code_list[$mod] ? true : false;
+    }
+}
---
+++ tests/ValidatorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Aichong\tests;
+
+use Aichong\Validator;
+
+class ValidatorTest extends TestCase
+{
+
+    public function testRequired()
+    {
+        $validator = new Validator();
+        $validator->validate(['test' => ''], ['test' => 'required'], ['test' => 'empty']);
+
+        $this->assertEquals($validator->message(), 'empty');
+    }
+
+    public function testEquals()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => '1'], ['test' => 'equals:1'], ['test' => 'noequals']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testLength()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => '123'], ['test' => 'length:3'], ['test' => 'nolength']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testUrl()
+    {
+        $validator = new Validator();
+        $validator->validate(['test' => '123'], ['test' => 'url'], ['test' => 'nourl']);
+
+        $this->assertEquals($validator->message(), 'nourl');
+    }
+
+    public function testMin()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => 5], ['test' => 'min:5'], ['test' => 'nomin']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testMax()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => 10], ['test' => 'max:10'], ['test' => 'nomax']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testEmail()
+    {
+        $validator = new Validator();
+        $validator->validate(['test' => 'zhangwj@5ichong'], ['test' => 'email'], ['test' => 'noemail']);
+
+        $this->assertEquals($validator->message(), 'noemail');
+    }
+
+    public function testPhone()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => '15821789646'], ['test' => 'phone'], ['test' => 'nophone']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testIp()
+    {
+        $validator = new Validator();
+        $ret = $validator->validate(['test' => '192.168.1.1'], ['test' => 'ip'], ['test' => 'noip']);
+
+        $this->assertTrue($ret);
+    }
+
+    public function testIdCard()
+    {
+        $validator = new Validator();
+        $validator->validate(['test' => '123456789098765432'], ['test' => 'id_card'], ['test' => 'no_idcard']);
+
+        $this->assertEquals($validator->message(), 'no_idcard');
+    }
+}
